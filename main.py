dd4fa892b9bf2f49ac01c6c18b07fc7e1c253f62
AxmetES <axmet-1984@mail.ru>
2020-01-30 21:29:56 +0600

    Changed image_url and add variable numb to  download random image Add remove_image() func to remove published image from folder deletad extra libs pprint changed names of functions : get_data() to get_images_data(), get_description() to get_num() and than was deleted Changed var names : vk_get_upload_url_api to vk_upload_api, vk_get_upload_params to vk_uploader_params, upload_url to uploader_url, upload_data to uploader_data, vk_save_image_api to vk_saver_api, vk_save_params to vk_saver_params, vk_wall_post_api to vk_wall_api, vk_wall_post_params to vk_wall_params end

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import requests
 import os
 from dotenv import load_dotenv
-import pprint
+from random import randint
 
 
 def make_directory(directory):
@@ -17,7 +17,7 @@ def get_image(image_link, filename, directory):
         file.write(response.content)
 
 
-def get_data(url):
+def get_image_data(url):
     response = requests.get(url=url)
     response.raise_for_status()
     image_data = response.json()
@@ -27,11 +27,6 @@ def get_data(url):
 def get_filename(image_link):
     head, tail = os.path.split(image_link)
     return tail
-
-
-def get_description(image_data):
-    description = image_data['alt']
-    return description
 
 
 def get_image_link(image_data):
@@ -46,6 +41,7 @@ def get_image_title(image_data):
 
 def get_upload_url(vk_get_groups_url, vk_get_group_params):
     response = requests.get(url=vk_get_groups_url, params=vk_get_group_params)
+    response.raise_for_status()
     upload_url = response.json()['response']['upload_url']
     return upload_url
 
@@ -56,6 +52,7 @@ def upload_image_to_group(upload_url, filename, directory):
     with open(full_file, 'rb') as file:
         files = {'photo': file}
         response = requests.post(upload_url, files=files)
+        response.raise_for_status()
         server_result = response.json()
     return server_result
 
@@ -83,8 +80,8 @@ def get_hash_data(upload_data):
 
 
 def get_id_owner_id(server_data):
-    media_id = 0
-    owner_id = 0
+    media_id = None
+    owner_id = None
     for item in server_data['response']:
         media_id = item['id']
         owner_id = item['owner_id']
@@ -93,46 +90,51 @@ def get_id_owner_id(server_data):
 
 def post_wall(vk_wall_post_api, vk_wall_post_params):
     response = requests.post(vk_wall_post_api, vk_wall_post_params)
-    post_id = response.json()
-    print(post_id)
+    response.raise_for_status()
+
+
+def remove_image(filename, directory):
+    full_name = directory + filename
+    os.remove(full_name)
 
 
 def main():
     load_dotenv(verbose=True)
-    client_id = os.getenv('CLIENT_ID')
     group_id = os.getenv('GROUP_ID')
     vk_access_token = os.getenv('ACCESS_TOKEN')
     directory = 'image/'
 
-    image_url = 'http://xkcd.com/info.0.json'
     make_directory(directory)
-    image_data = get_data(image_url)
-    description = get_description(image_data)
+
+    numb = randint(1, 2261)
+    image_url = f'http://xkcd.com/{numb}/info.0.json'
+    image_data = get_image_data(image_url)
     image_title = get_image_title(image_data)
     image_link = get_image_link(image_data)
 
     filename = get_filename(image_link)
     get_image(image_link, filename, directory)
 
-    vk_get_upload_url_api = 'https://api.vk.com/method/photos.getWallUploadServer'
-    vk_get_upload_params = {'access_token': vk_access_token, 'caption': description, 'v': 5.103}
-    upload_url = get_upload_url(vk_get_upload_url_api, vk_get_upload_params)
+    vk_uploader_api = 'https://api.vk.com/method/photos.getWallUploadServer'
+    vk_uploader_params = {'access_token': vk_access_token, 'caption': image_title, 'v': 5.103}
+    uploader_url = get_upload_url(vk_uploader_api, vk_uploader_params)
 
-    upload_data = upload_image_to_group(upload_url, filename, directory)
-    server_data = get_server_data(upload_data)
-    photo_data = get_photo_data(upload_data)
-    hash_data = get_hash_data(upload_data)
+    uploader_data = upload_image_to_group(uploader_url, filename, directory)
+    server_data = get_server_data(uploader_data)
+    photo_data = get_photo_data(uploader_data)
+    hash_data = get_hash_data(uploader_data)
 
-    vk_save_image_api = 'https://api.vk.com/method/photos.saveWallPhoto'
-    vk_save_params = {'access_token': vk_access_token, 'photo': photo_data, 'server': server_data,
-                      'hash': hash_data, 'v': 5.103}
-    server_data = save_image_to_group(vk_save_image_api, vk_save_params)
+    vk_saver_api = 'https://api.vk.com/method/photos.saveWallPhoto'
+    vk_saver_params = {'access_token': vk_access_token, 'photo': photo_data, 'server': server_data,
+                       'hash': hash_data, 'v': 5.103}
+    server_data = save_image_to_group(vk_saver_api, vk_saver_params)
     media_id, owner_id = get_id_owner_id(server_data)
 
-    vk_wall_post_api = 'https://api.vk.com/method/wall.post'
-    vk_wall_post_params = {'access_token': vk_access_token, 'owner_id': group_id, 'from_group': 1,
-                           'message': description, 'attachments': f'photo{owner_id}_{media_id}', 'v': 5.103}
-    post_wall(vk_wall_post_api, vk_wall_post_params)
+    vk_wall_api = 'https://api.vk.com/method/wall.post'
+    vk_wall_params = {'access_token': vk_access_token, 'owner_id': group_id, 'from_group': 1,
+                      'message': image_title, 'attachments': f'photo{owner_id}_{media_id}', 'v': 5.103}
+    post_wall(vk_wall_api, vk_wall_params)
+    remove_image(filename, directory)
 
 
 if __name__ == '__main__':
